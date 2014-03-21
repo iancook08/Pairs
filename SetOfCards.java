37db68132f365a8484e72ed5734287aeb7eaa8ac
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-03-22 01:07:23 +0800

    Update SetOfCards.java

--- SetOfCards.java
+++ SetOfCards.java
@@ -1,16 +1,83 @@
 public class SetOfCards {
 
-    final static int cardsNumber = 16;
+    final static int cardsPerLine = 4;
+    final static int cardsPerRow = 4;
+	private Card[][] card_matrix = new Card[cardsPerLine][cardsPerRow];
+	private int numberOfTries;
     
-    public static void main (String[] args)
-    {
-      Card[] card_list = new Card[cardsNumber];
-      
-      for(int i=0 ; i<cardsNumber ; i=i+2) {
-          card_list[i]= new Card();
-          card_list[i+1]= new Card(card_list[i].getCardString());
-          System.out.println("Card "+(i+1) + ": "+ card_list[i].getCardString());
-          System.out.println("Card "+(i+2) + ": "+ card_list[i+1].getCardString());
-      }
+    SetOfCards () {
+    	int j=0;
+    	for (int i=0 ; i<cardsPerRow ; i++) {   
+	    	for(j=0 ; j<cardsPerLine ; j=j+2) {
+	    		card_matrix[i][j]= new Card();
+	    		card_matrix[i][j+1]= new Card(card_matrix[i][j].getCardString());
+	    	}
+    	}
+    	this.numberOfTries = 0;
+    }
+    
+    public void shakeMatrix () {
+    	/*
+    	 *  Let t be a n length table to shake
+		 *	for i from n-1 to 2 included, do:
+		 *	j = take a random number between 0 and i included
+		 *	exchange t[i] and t[j]
+		 *	end for
+    	 */
+    	int rand;
+    	Card temp;
+    	for(int i= cardsPerLine*cardsPerRow -1 ; i>=2 ; i--) {
+    		rand = (int) (Math.random() * (i+1));
+    		temp = card_matrix[(int) i/cardsPerLine][i%cardsPerLine];
+    		card_matrix[(int) i/cardsPerLine][i%cardsPerLine]=card_matrix[(int) rand/cardsPerLine][rand%cardsPerLine];
+    		card_matrix[(int) rand/cardsPerLine][rand%cardsPerLine] = temp;
+    	}
+    }
+    
+    public String[][] getMatrix() {
+    	String[][] matrix = new String [cardsPerLine][cardsPerRow];
+    	int j=0;
+    	for (int i=0 ; i<cardsPerRow ; i++) {   
+	    	for(j=0 ; j<cardsPerLine ; j=j+2) {
+	    		if (card_matrix[i][j].isHidden())
+	    			matrix[i][j]="null";
+	    		else
+	    			matrix[i][j]=card_matrix[i][j].getCardString();
+	    	}
+    	}
+    	return matrix;
+    }
+    
+    public int getNumberOfTries() {
+    	return this.numberOfTries;
+    }
+    
+    public boolean win() {
+		int j=0;
+    	for (int i=0 ; i<cardsPerRow ; i++) {   
+	    	for(j=0 ; j<cardsPerLine ; j++) {
+	    		if (card_matrix[i][j].isHidden())
+	    			return false;
+	    	}
+    	}
+    	return true;
+    }
+    
+    // public void click1 (int x, int y) (to do)
+    // public void click2 (int x, int y) (to do)
+
+    
+    public String toString() {
+    	String print="";
+    	int j=0;
+    	for (int i=0 ; i<cardsPerRow ; i++) {   
+	    	for(j=0 ; j<cardsPerLine ; j++) {
+	    		if (card_matrix[i][j].isHidden())
+	    		print= print + "Card ("+(i+1) + ","+ (j+1)+ "): "+ card_matrix[i][j].getCardString()+" (hidden)\n";
+	    		else
+		    	print= print + "Card ("+(i+1) + ","+ (j+1)+ "): "+ card_matrix[i][j].getCardString()+" (visible)\n";
+	    	}
+    	}
+    	return print;
     }
 }
