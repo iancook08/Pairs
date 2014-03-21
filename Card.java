d8a844593d157f8954b5ba1f5a1cdcb1f0456e4e
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-03-22 01:12:20 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,38 +1,38 @@
 public class Card {
 	private static String possibleLetters = "abcdefghijklmnopqrstuvwxyz";
-    private String cardString;
-    private boolean hidden;
+    	private String cardString;
+    	private boolean hidden;
     
-    Card () {
+    	Card () {
 		int randomNumber = (int) (Math.random() * (possibleLetters.length()));
 		char character = possibleLetters.charAt(randomNumber);
-	    this.cardString = Character.toString(character);
+	    	this.cardString = Character.toString(character);
 		this.hidden = true;
 		possibleLetters = possibleLetters.substring(0, randomNumber) + possibleLetters.substring(randomNumber+1);
-    }
+    	}
     
-    Card (String cardString) {
+	Card (String cardString) {
 		this.cardString = cardString;
 		this.hidden = true;
-    }
+	}
     
-    public boolean isHidden () {
-        return hidden;
-    }
+    	public boolean isHidden () {
+        	return hidden;
+    	}
     
-    public void hide () {
-    	this.hidden = true;   
-    }
+    	public void hide () {
+    		this.hidden = true;   
+    	}
+	
+	public void discover () {
+    		this.hidden = false;   
+    	}
 
-    public void discover () {
-    	this.hidden = false;   
-    }
+    	public String getCardString () {
+    		return this.cardString;
+    	}
 
-    public String getCardString () {
-    	return this.cardString;
-    }
-
-    public String toString() {
-        return ("Card "+cardString+", hidden: "+ this.hidden);
-    }
+    	public String toString() {
+        	return ("Card "+cardString+", hidden: "+ this.hidden);
+    	}
 }
