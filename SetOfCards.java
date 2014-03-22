02c3650096bac7e13b6daac2545a6ec2603ff666
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-03-22 21:42:08 +0800

    Update SetOfCards.java

--- SetOfCards.java
+++ SetOfCards.java
@@ -2,18 +2,20 @@ public class SetOfCards {
 
     	final static int cardsPerLine = 4;
     	final static int cardsPerRow = 4;
-	private Card[][] card_matrix = new Card[cardsPerLine][cardsPerRow];
-	private int numberOfTries;
+    	private Card[][] card_matrix = new Card[cardsPerLine][cardsPerRow];
+    	private int numberOfTries;
+    	private Card[] tempCard = new Card[2];
+    	private boolean clock;
     
     	SetOfCards () {
-	    	int j=0;
 	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(j=0 ; j<cardsPerLine ; j=j+2) {
+		    	for(int j=0 ; j<cardsPerLine ; j=j+2) {
 		    		card_matrix[i][j]= new Card();
 		    		card_matrix[i][j+1]= new Card(card_matrix[i][j].getCardString());
 		    	}
 	    	}
 	    	this.numberOfTries = 0;
+	    	this.clock=false;
     	}
     
     	public void shakeMatrix () {
@@ -36,9 +38,8 @@ public class SetOfCards {
     
     	public String[][] getMatrix() {
 	    	String[][] matrix = new String [cardsPerLine][cardsPerRow];
-	    	int j=0;
 	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(j=0 ; j<cardsPerLine ; j=j+2) {
+		    	for(int j=0 ; j<cardsPerLine ; j++) {
 		    		if (card_matrix[i][j].isHidden())
 		    			matrix[i][j]="null";
 		    		else
@@ -52,31 +53,67 @@ public class SetOfCards {
     		return this.numberOfTries;
     	}
     
-	public boolean win() {
-		int j=0;
+    	public boolean win() {
 	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-			for(j=0 ; j<cardsPerLine ; j++) {
+			for(int j=0 ; j<cardsPerLine ; j++) {
 		    		if (card_matrix[i][j].isHidden())
 		    		return false;
 		    	}
 	    	}
 	    	return true;
-	}
-    
-    
-    	// public void click1 (int x, int y) (to do)
-    	// public void click2 (int x, int y) (to do)
-
+    	}
+      
+    	public boolean click1 (int x, int y) {
+    		if (x<0 || x>= cardsPerLine || y<0 || y>=cardsPerRow)
+    			return false;
+    		else if (!card_matrix[x][y].isHidden())
+    			return false;
+    		else {
+    			card_matrix[x][y].discover();
+    			tempCard[0] = card_matrix[x][y];
+    			return true;
+    		}
+    	}
+    	
+    	public boolean click2 (int x, int y) {
+    		if (x<0 || x>= cardsPerLine || y<0 || y>=cardsPerRow)
+    			return false;
+    		else if (!card_matrix[x][y].isHidden())
+    			return false;
+    		else {
+				card_matrix[x][y].discover();
+    			numberOfTries++;
+    			if (card_matrix[x][y].getCardString().compareTo(tempCard[0].getCardString())==0) {
+    				tempCard[0]=null;
+    			}
+    			else {
+    				clock=true;
+    				tempCard[1]=card_matrix[x][y];
+    			}
+    			return true;
+    		}
+    	}
+    	
+    	public boolean getClock() {
+    		return clock;
+    	}
+    	
+    	public void endClock() {
+    		clock=false;
+    		tempCard[0].hide();
+    		tempCard[1].hide();
+    		tempCard[0]=null;
+    		tempCard[1]=null;
+    	}
     
     	public String toString() {
 	    	String print="";
-	    	int j=0;
 	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(j=0 ; j<cardsPerLine ; j++) {
+		    	for(int j=0 ; j<cardsPerLine ; j++) {
 		    		if (card_matrix[i][j].isHidden())
-		    		print= print + "Card ("+(i+1) + ","+ (j+1)+ "): "+ card_matrix[i][j].getCardString()+" (hidden)\n";
+		    		print= print + "Card ("+i + ","+ j+ "): "+ card_matrix[i][j].getCardString()+" (hidden)\n";
 		    		else
-			    	print= print + "Card ("+(i+1) + ","+ (j+1)+ "): "+ card_matrix[i][j].getCardString()+" (visible)\n";
+			    	print= print + "Card ("+i + ","+ j+ "): "+ card_matrix[i][j].getCardString()+" (visible)\n";
 		    	}
 	    	}
 	    	return print;
